f048bf9467e7a6c939ee933678009801d42b2049
yangkles <yangkles@naver.com>
2025-10-26 17:43:46 +0900

    [feat] 풀이 주석 추가

--- Week2/주식가격/yhyang.py
+++ Week2/주식가격/yhyang.py
@@ -1,16 +1,23 @@
+# 주어진 각 시점의 주식 가격이 얼마 동안 떨지지 않았는지 구하는 문제
+
 def solution(prices):
     n = len(prices)
     answer = [0] * n
     stack = []
 
+# n은 주가 의 갯수, answer은 각 시점별 결과 저장 리스트, stack은 주가가 떨어지지 않은 인덱스 들을 저장하는 스텍
+
     for i in range(n):
         while stack and prices[stack[-1]] > prices[i]:
             idx = stack.pop()
+            # 주가가 떨어진 시점의 인덱스를 꺼냄
             answer[idx] = i - idx
+            # 가격이 유지된 시간을 기록
         stack.append(i)
-
     while stack:
         idx = stack.pop()
         answer[idx] = n - 1 - idx
+        # 가격이 끝까지 유지된 시간을 처리
 
     return answer
+
--- Week2/카드_뭉치/yhyang.py
+++ Week2/카드_뭉치/yhyang.py
@@ -3,8 +3,28 @@ def solution(cards1, cards2, goal):
     for i in goal:
         if cards1 and i == cards1[0]:
             cards1.pop(0)
+        # i번 단어가 1번 배열안에 있으면 꺼냄
         elif cards2 and i == cards2[0]:
             cards2.pop(0)
+        # i번 단어가 2번 배열안에 있으면 꺼냄
+        else:
+            answer = "No"
+        # 없다면 "No"
+    return answer
+
+# 시간 복잡도 O(n)
+
+# 시간 복잡도 O(1)로 하는 방법
+
+from collections import deque
+
+def solution2(cards1, cards2, goal):
+    cards1, cards2 = deque(cards1), deque(cards2)
+    for word in goal:
+        if cards1 and word == cards1[0]:
+            cards1.popleft()
+        elif cards2 and word == cards2[0]:
+            cards2.popleft()
         else:
             answer = "No"
     return answer
